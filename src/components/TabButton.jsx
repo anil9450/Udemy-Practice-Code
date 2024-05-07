1ff00be58691d8f19fd43166673c51df8367fea9
ANIL KUMAR SINGH KUSHWAHA <90541938+anil9450@users.noreply.github.com>
2024-05-07 14:14:52 +0530

    Tab Button Children at the place of props

--- src/components/TabButton.jsx
+++ src/components/TabButton.jsx
@@ -1,5 +1,5 @@
-export default function TabButton(props) {
+export default function TabButton({children}) {
     return(
-        <li><button>{props.children}</button></li>
+        <li><button>{children}</button></li>
     )
 }
